2851bc95c4b7311c90805a7286eaa68a0bd93f17
Garrett McTear <garrett.mctear@gmail.com>
2018-06-20 11:26:19 -0600

    Initial commit

---
+++ Kata/Kata.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace ConsoleApp1
+{
+    public class Kata
+    {
+        public static int FindEvenIndex(int[] arr)
+        {
+            var leftSum = 0;
+            var rightSum = arr.Sum();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                rightSum = rightSum - arr[i];
+
+                if (leftSum == rightSum)
+                {
+                    return i;
+                }
+
+                leftSum = leftSum + arr[i];
+            }
+
+            return -1;
+        }
+
+        public static int DuplicateCount(string str)
+        {
+            return str.ToLower().GroupBy(c => c).Where(g => g.Count() > 1).Count();
+        }
+
+        public static bool is_valid_IP1(string IpAddres)
+        {
+            if (String.IsNullOrWhiteSpace(IpAddres) || IpAddres.Contains(' '))
+            {
+                return false;
+            }
+
+            var octets = IpAddres.Split('.');
+
+            if (octets.Length != 4)
+            {
+                return false;
+            }
+
+            foreach (var octet in octets)
+            {
+                try
+                {
+                    if (octet[0].Equals('0'))
+                    {
+                        return false;
+                    }
+
+                    var number = int.Parse(octet);
+
+                    if (number > 255 || number < 0)
+                    {
+                        return false;
+                    }
+                }
+                catch (Exception e)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
+        public static bool is_valid_IP(string IpAddres)
+        {
+            return Regex.IsMatch(IpAddres, @"^[1-2]?(?<!0)[0-9]{0,2}\.[1-2]?(?<!0)[0-9]{0,2}\.[1-2]?(?<!0)[0-9]{0,2}\.[1-2]?(?<!0)[0-9]{0,2}$");
+        }
+
+        public static string SongDecoder(string input)
+        {
+            var words = input.Split(new string[] {"WUB"}, StringSplitOptions.None);
+
+            var original = string.Empty;
+            foreach (var word in words)
+            {
+                original = original + " " + word;
+            }
+
+            // var original = words.Aggregate(string.Empty, (current, word) => current + " " + word);
+
+            return Regex.Replace(original, @"\s+", " ").Trim();
+        }
+    }
+}
---
+++ Kata/Tests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+
+namespace ConsoleApp1
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void GenericTests()
+        {
+            Assert.AreEqual(3, Kata.FindEvenIndex(new int[] { 1, 2, 3, 4, 3, 2, 1 }));
+            Assert.AreEqual(1, Kata.FindEvenIndex(new int[] { 1, 100, 50, -51, 1, 1 }));
+            Assert.AreEqual(-1, Kata.FindEvenIndex(new int[] { 1, 2, 3, 4, 5, 6 }));
+            Assert.AreEqual(3, Kata.FindEvenIndex(new int[] { 20, 10, 30, 10, 10, 15, 35 }));
+        }
+
+        [Test]
+        public void KataTests()
+        {
+            Assert.AreEqual(0, Kata.DuplicateCount(""));
+            Assert.AreEqual(0, Kata.DuplicateCount("abcde"));
+            Assert.AreEqual(2, Kata.DuplicateCount("aabbcde"));
+            Assert.AreEqual(2, Kata.DuplicateCount("aabBcde"), "should ignore case");
+            Assert.AreEqual(1, Kata.DuplicateCount("Indivisibility"));
+            Assert.AreEqual(2, Kata.DuplicateCount("Indivisibilities"), "characters may not be adjacent");
+        }
+
+        [Test]
+        public void TestCases()
+        {
+            Assert.AreEqual(Kata.is_valid_IP("12.255.56.1"), true);
+            Assert.AreEqual(Kata.is_valid_IP(""), false);
+            Assert.AreEqual(Kata.is_valid_IP("abc.def.ghi.jkl"), false);
+            Assert.AreEqual(Kata.is_valid_IP("123.456.789.0"), false);
+            Assert.AreEqual(Kata.is_valid_IP("12.34.56"), false);
+            Assert.AreEqual(Kata.is_valid_IP("12.34.56 .1"), false);
+            Assert.AreEqual(Kata.is_valid_IP("12.34.56.-1"), false);
+            Assert.AreEqual(Kata.is_valid_IP("123.045.067.089"), false);
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.AreEqual("ABC", Kata.SongDecoder("WUBWUBABCWUB"));
+        }
+
+        [Test]
+        public void Test2()
+        {
+            Assert.AreEqual("R L", Kata.SongDecoder("RWUBWUBWUBLWUB"));
+        }
+
+        [Test]
+        public void Test3()
+        {
+            Assert.AreEqual("WE ARE THE CHAMPIONS MY FRIEND", Kata.SongDecoder("WUBWEWUBAREWUBWUBTHEWUBCHAMPIONSWUBMYWUBFRIENDWUB"));
+        }
+
+
+    }
+}
